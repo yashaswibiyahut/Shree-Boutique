2bc54ca9e262162ebce57cdbbdc701ca5ed4a7ec
Yashaswi Biyahut <yashaswibiyahut25@gmail.com>
2022-07-10 00:48:37 +0530

    change

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/demogit'));
+app.use(express.static('./dist/demo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/demogit/'}),
+    res.sendFile('index.html', {root: 'dist/demo/'}),
 );
 
 // Start the app by listening on the default Heroku port
